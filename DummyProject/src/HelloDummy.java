0d65032a33cd1a07495a18d66684e279046f937b
varanh <harikrishna.varanasi@gmail.com>
2015-10-17 23:01:08 +0100

    Changed to Main Man

--- DummyProject/src/HelloDummy.java
+++ DummyProject/src/HelloDummy.java
@@ -2,7 +2,7 @@
 public class HelloDummy {
 
 	public static void main(String[] args) {
-		NewDummy dummy = new NewDummy(45, "John Doe");
+		NewDummy dummy = new NewDummy(45, "Main Man");
 		System.out.println("Hello " + dummy.getName() + "!!");
 	}
 
