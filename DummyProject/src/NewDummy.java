9f71b79a6c328de4ef9ceb3d44f67dc414b4cbbd
varanh <harikrishna.varanasi@gmail.com>
2015-10-17 22:50:56 +0100

    Reformatted

--- DummyProject/src/NewDummy.java
+++ DummyProject/src/NewDummy.java
@@ -1,19 +1,19 @@
 
 public class NewDummy {
 
-		private int age;
-		private String name = "Mr.Dummy";
-		
-		public NewDummy(int Age, String fullName) {
-				this.age = Age;
-				this.name = fullName;
-		}		
-		
-		public String getName(){
-			return this.name;
-		}
-		
-		public int getAge(){
-			return this.age;
-		}
+	private int age;
+	private String name = "Mr.Dummy";
+
+	public NewDummy(int Age, String fullName) {
+		this.age = Age;
+		this.name = fullName;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
 }
